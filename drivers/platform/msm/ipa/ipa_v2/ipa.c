11333be904f557e2f0f028da9a1da53212df780d
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-06-07 22:54:58 +0700

    fixup: ipa_v2: Compile out a unused variable
    Fixup from commit 09d04331f866ee2d66aa916d3578c6c2d18f6012
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -5053,8 +5053,10 @@ int ipa_plat_drv_probe(struct platform_device *pdev_p,
 */
 int ipa2_ap_suspend(struct device *dev)
 {
-	int i;
-
+#if 0
+    int i;
+#endif
+    
 	IPADBG("Enter...\n");
 
 #if 0
