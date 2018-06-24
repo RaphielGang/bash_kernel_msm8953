805d10143298d54261c8cfa23b5f728eea8c22b8
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-06-24 17:05:00 +0700

    DRIVERS: power: qpnp-fg: Compile out some wakelocks
    It's annoying too
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -522,15 +522,19 @@ struct fg_chip {
 	struct power_supply	*batt_psy;
 	struct power_supply	*usb_psy;
 	struct power_supply	*dc_psy;
+	#if 0
 	struct fg_wakeup_source	memif_wakeup_source;
+	#endif
 	struct fg_wakeup_source	profile_wakeup_source;
 	struct fg_wakeup_source	empty_check_wakeup_source;
 	struct fg_wakeup_source	resume_soc_wakeup_source;
 	struct fg_wakeup_source	gain_comp_wakeup_source;
 	struct fg_wakeup_source	capacity_learning_wakeup_source;
 	bool			first_profile_loaded;
+	#if 0
 	struct fg_wakeup_source	update_temp_wakeup_source;
 	struct fg_wakeup_source	update_sram_wakeup_source;
+	#endif
 	bool			fg_restarting;
 	bool			profile_loaded;
 	bool			soc_reporting_ready;
@@ -959,7 +963,9 @@ static int fg_req_and_wait_access(struct fg_chip *chip, int timeout)
 			pr_err("failed to set mem access bit\n");
 			return -EIO;
 		}
+		#if 0
 		fg_stay_awake(&chip->memif_wakeup_source);
+		#endif
 	}
 
 wait:
@@ -986,7 +992,9 @@ static int fg_release_access(struct fg_chip *chip)
 
 	rc = fg_masked_write(chip, MEM_INTF_CFG(chip),
 			RIF_MEM_ACCESS_REQ, 0, 1);
+	#if 0
 	fg_relax(&chip->memif_wakeup_source);
+	#endif
 	reinit_completion(&chip->sram_access_granted);
 
 	return rc;
@@ -1758,7 +1766,9 @@ static int fg_interleaved_mem_read(struct fg_chip *chip, u8 *val, u16 address,
 		return -EINVAL;
 	}
 
+	#if 0
 	fg_stay_awake(&chip->memif_wakeup_source);
+	#endif
 	address = ((orig_address + offset) / 4) * 4;
 	offset = (orig_address + offset) % 4;
 
@@ -1852,7 +1862,9 @@ out:
 
 	mutex_unlock(&chip->rw_lock);
 exit:
+	#if 0
 	fg_relax(&chip->memif_wakeup_source);
+	#endif
 	return rc;
 }
 
@@ -1874,7 +1886,9 @@ static int fg_interleaved_mem_write(struct fg_chip *chip, u8 *val, u16 address,
 		return -EINVAL;
 	}
 
+	#if 0
 	fg_stay_awake(&chip->memif_wakeup_source);
+	#endif
 	address = ((orig_address + offset) / 4) * 4;
 	offset = (orig_address + offset) % 4;
 
@@ -1924,7 +1938,9 @@ out:
 	}
 
 	mutex_unlock(&chip->rw_lock);
+	#if 0
 	fg_relax(&chip->memif_wakeup_source);
+	#endif
 	return rc;
 }
 
@@ -2563,7 +2579,9 @@ static int update_sram_data(struct fg_chip *chip, int *resched_ms)
 	int64_t temp;
 	int battid_valid = fg_is_batt_id_valid(chip);
 
+	#if 0
 	fg_stay_awake(&chip->update_sram_wakeup_source);
+	#endif
 	if (chip->fg_restarting)
 		goto resched;
 
@@ -2668,7 +2686,9 @@ resched:
 		*resched_ms = SRAM_PERIOD_NO_ID_UPDATE_MS;
 	}
 out:
+	#if 0
 	fg_relax(&chip->update_sram_wakeup_source);
+	#endif
 	return rc;
 }
 
@@ -2800,7 +2820,9 @@ static void update_temp_data(struct work_struct *work)
 	if (chip->fg_restarting)
 		goto resched;
 
+	#if 0
 	fg_stay_awake(&chip->update_temp_wakeup_source);
+	#endif
 	if (chip->sw_rbias_ctrl) {
 		rc = fg_mem_masked_write(chip, EXTERNAL_SENSE_SELECT,
 				BATT_TEMP_CNTRL_MASK,
@@ -2885,7 +2907,9 @@ out:
 		if (rc)
 			pr_err("failed to write BATT_TEMP_OFF rc=%d\n", rc);
 	}
+	#if 0
 	fg_relax(&chip->update_temp_wakeup_source);
+	#endif
 
 resched:
 	queue_delayed_work(system_power_efficient_wq,
@@ -7556,10 +7580,14 @@ static void fg_cleanup(struct fg_chip *chip)
 	mutex_destroy(&chip->ima_recovery_lock);
 	wakeup_source_trash(&chip->resume_soc_wakeup_source.source);
 	wakeup_source_trash(&chip->empty_check_wakeup_source.source);
+	#if 0
 	wakeup_source_trash(&chip->memif_wakeup_source.source);
+	#endif
 	wakeup_source_trash(&chip->profile_wakeup_source.source);
+	#if 0
 	wakeup_source_trash(&chip->update_temp_wakeup_source.source);
 	wakeup_source_trash(&chip->update_sram_wakeup_source.source);
+	#endif
 	wakeup_source_trash(&chip->gain_comp_wakeup_source.source);
 	wakeup_source_trash(&chip->capacity_learning_wakeup_source.source);
 	wakeup_source_trash(&chip->esr_extract_wakeup_source.source);
@@ -8770,14 +8798,18 @@ static int fg_probe(struct spmi_device *spmi)
 
 	wakeup_source_init(&chip->empty_check_wakeup_source.source,
 			"qpnp_fg_empty_check");
+	#if 0
 	wakeup_source_init(&chip->memif_wakeup_source.source,
 			"qpnp_fg_memaccess");
+	#endif
 	wakeup_source_init(&chip->profile_wakeup_source.source,
 			"qpnp_fg_profile");
+	#if 0
 	wakeup_source_init(&chip->update_temp_wakeup_source.source,
 			"qpnp_fg_update_temp");
 	wakeup_source_init(&chip->update_sram_wakeup_source.source,
 			"qpnp_fg_update_sram");
+	#endif
 	wakeup_source_init(&chip->resume_soc_wakeup_source.source,
 			"qpnp_fg_set_resume_soc");
 	wakeup_source_init(&chip->gain_comp_wakeup_source.source,
@@ -8998,10 +9030,14 @@ of_init_fail:
 	mutex_destroy(&chip->ima_recovery_lock);
 	wakeup_source_trash(&chip->resume_soc_wakeup_source.source);
 	wakeup_source_trash(&chip->empty_check_wakeup_source.source);
+	#if 0
 	wakeup_source_trash(&chip->memif_wakeup_source.source);
+	#endif
 	wakeup_source_trash(&chip->profile_wakeup_source.source);
+	#if 0
 	wakeup_source_trash(&chip->update_temp_wakeup_source.source);
 	wakeup_source_trash(&chip->update_sram_wakeup_source.source);
+	#endif
 	wakeup_source_trash(&chip->gain_comp_wakeup_source.source);
 	wakeup_source_trash(&chip->capacity_learning_wakeup_source.source);
 	wakeup_source_trash(&chip->esr_extract_wakeup_source.source);
