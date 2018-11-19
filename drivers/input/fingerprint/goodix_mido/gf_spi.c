7538eb6f231de24a7dfa0c61befbdb1dbad5ce26
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-11-20 02:21:31 +0700

    input: fingerprint: goodix_mido: ommit clock control code
    We don't use clock control
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/input/fingerprint/goodix_mido/gf_spi.c
+++ drivers/input/fingerprint/goodix_mido/gf_spi.c
@@ -112,142 +112,6 @@ static void gf_disable_irq(struct gf_dev *gf_dev)
 		pr_warn("IRQ has been disabled.\n");
 }
 
-#ifdef AP_CONTROL_CLK
-static long spi_clk_max_rate(struct clk *clk, unsigned long rate)
-{
-	long lowest_available, nearest_low, step_size, cur;
-	long step_direction = -1;
-	long guess = rate;
-	int max_steps = 10;
-
-	cur = clk_round_rate(clk, rate);
-	if (cur == rate)
-		return rate;
-
-	/* if we got here then: cur > rate */
-	lowest_available = clk_round_rate(clk, 0);
-	if (lowest_available > rate)
-		return -EINVAL;
-
-	step_size = (rate - lowest_available) >> 1;
-	nearest_low = lowest_available;
-
-	while (max_steps-- && step_size) {
-		guess += step_size * step_direction;
-		cur = clk_round_rate(clk, guess);
-
-		if ((cur < rate) && (cur > nearest_low))
-			nearest_low = cur;
-		/*
-		 * if we stepped too far, then start stepping in the other
-		 * direction with half the step size
-		 */
-		if (((cur > rate) && (step_direction > 0)) ||
-		    ((cur < rate) && (step_direction < 0))) {
-			step_direction = -step_direction;
-			step_size >>= 1;
-		}
-	}
-	return nearest_low;
-}
-
-static void spi_clock_set(struct gf_dev *gf_dev, int speed)
-{
-	long rate;
-	int rc;
-
-	rate = spi_clk_max_rate(gf_dev->core_clk, speed);
-	if (rate < 0) {
-		pr_info("%s: no match found for requested clock frequency:%d",
-			__func__, speed);
-		return;
-	}
-
-	rc = clk_set_rate(gf_dev->core_clk, rate);
-}
-
-static int gfspi_ioctl_clk_init(struct gf_dev *data)
-{
-	pr_debug("%s: enter\n", __func__);
-
-	data->clk_enabled = 0;
-	data->core_clk = clk_get(&data->spi->dev, "core_clk");
-	if (IS_ERR_OR_NULL(data->core_clk)) {
-		pr_err("%s: fail to get core_clk\n", __func__);
-		return -EPERM;
-	}
-	data->iface_clk = clk_get(&data->spi->dev, "iface_clk");
-	if (IS_ERR_OR_NULL(data->iface_clk)) {
-		pr_err("%s: fail to get iface_clk\n", __func__);
-		clk_put(data->core_clk);
-		data->core_clk = NULL;
-		return -ENOENT;
-	}
-	return 0;
-}
-
-static int gfspi_ioctl_clk_enable(struct gf_dev *data)
-{
-	int err;
-
-	pr_debug("%s: enter\n", __func__);
-
-	if (data->clk_enabled)
-		return 0;
-
-	err = clk_prepare_enable(data->core_clk);
-	if (err) {
-		pr_err("%s: fail to enable core_clk\n", __func__);
-		return -EPERM;
-	}
-
-	err = clk_prepare_enable(data->iface_clk);
-	if (err) {
-		pr_err("%s: fail to enable iface_clk\n", __func__);
-		clk_disable_unprepare(data->core_clk);
-		return -ENOENT;
-	}
-
-	data->clk_enabled = 1;
-
-	return 0;
-}
-
-static int gfspi_ioctl_clk_disable(struct gf_dev *data)
-{
-	pr_debug("%s: enter\n", __func__);
-
-	if (!data->clk_enabled)
-		return 0;
-
-	clk_disable_unprepare(data->core_clk);
-	clk_disable_unprepare(data->iface_clk);
-	data->clk_enabled = 0;
-
-	return 0;
-}
-
-static int gfspi_ioctl_clk_uninit(struct gf_dev *data)
-{
-	pr_debug("%s: enter\n", __func__);
-
-	if (data->clk_enabled)
-		gfspi_ioctl_clk_disable(data);
-
-	if (!IS_ERR_OR_NULL(data->core_clk)) {
-		clk_put(data->core_clk);
-		data->core_clk = NULL;
-	}
-
-	if (!IS_ERR_OR_NULL(data->iface_clk)) {
-		clk_put(data->iface_clk);
-		data->iface_clk = NULL;
-	}
-
-	return 0;
-}
-#endif
-
 static long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	struct gf_dev *gf_dev = &gf;
@@ -255,9 +119,6 @@ static long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	int retval = 0;
 	int i;
 
-#ifdef AP_CONTROL_CLK
-	unsigned int speed = 0;
-#endif
 	FUNC_ENTRY();
 	if (_IOC_TYPE(cmd) != GF_IOC_MAGIC)
 		return -ENODEV;
@@ -295,21 +156,6 @@ static long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		gf_enable_irq(gf_dev);
 		break;
 	case GF_IOC_SETSPEED:
-#ifdef AP_CONTROL_CLK
-		retval = __get_user(speed, (u32 __user *)arg);
-		if (retval == 0) {
-			if (speed > 8 * 1000 * 1000)
-				pr_warn("Set speed:%d is larger than 8Mbps.\n",
-					speed);
-			else
-				spi_clock_set(gf_dev, speed);
-		} else
-			pr_warn("Failed to get speed from user. retval = %d\n",
-				retval);
-
-#else
-		pr_info("This kernel doesn't support control clk in AP\n");
-#endif
 		break;
 	case GF_IOC_RESET:
 		gf_hw_reset(gf_dev, 3);
@@ -351,18 +197,8 @@ static long gf_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		break;
 	case GF_IOC_CLK_READY:
-#ifdef AP_CONTROL_CLK
-		gfspi_ioctl_clk_enable(gf_dev);
-#else
-		pr_info("Doesn't support control clock.\n");
-#endif
 		break;
 	case GF_IOC_CLK_UNREADY:
-#ifdef AP_CONTROL_CLK
-		gfspi_ioctl_clk_disable(gf_dev);
-#else
-		pr_info("Doesn't support control clock.\n");
-#endif
 		break;
 	case GF_IOC_PM_FBCABCK:
 		__put_user(gf_dev->fb_black, (u8 __user *)arg);
@@ -632,17 +468,6 @@ static int gf_probe(struct platform_device *pdev)
 			status = -ENOMEM;
 			goto error;
 		}
-#ifdef AP_CONTROL_CLK
-		pr_info("Get the clk resource.\n");
-		/* Enable spi clock */
-		if (gfspi_ioctl_clk_init(gf_dev))
-			goto gfspi_probe_clk_init_failed;
-
-		if (gfspi_ioctl_clk_enable(gf_dev))
-			goto gfspi_probe_clk_enable_failed;
-
-		spi_clock_set(gf_dev, 4.8 * 1000 * 1000);
-#endif
 
 		INIT_WORK(&gf_dev->fb_work, fb_state_worker);
 		gf_dev->notifier = goodix_noti_block;
@@ -666,11 +491,6 @@ error:
 		clear_bit(MINOR(gf_dev->devt), minors);
 		mutex_unlock(&device_list_lock);
 
-#ifdef AP_CONTROL_CLK
-	gfspi_probe_clk_enable_failed:
-		gfspi_ioctl_clk_uninit(gf_dev);
-	gfspi_probe_clk_init_failed:
-#endif
 		if (gf_dev->input != NULL)
 			input_unregister_device(gf_dev->input);
 	}
